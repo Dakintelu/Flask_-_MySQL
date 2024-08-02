29bc970a1a7d66367e24ccfbcb3ebb771d93af27
Miraclez00 <168011350+Miraclez00@users.noreply.github.com>
2024-08-02 23:27:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">       
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library Records</title>
+    <title>Library Records</title>     
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
